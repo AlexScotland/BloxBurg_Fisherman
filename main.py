a76595e7d23fbd0be76c661c785ab9e2c85b4cdf
Dug <alex@scotland.ca>
2019-10-23 15:46:41 -0400

    Second revision - white detection

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from pynput.mouse import Button, Controller
+mouse = Controller()
+from pynput.keyboard import Key, Controller
+keyboard = Controller()
 from pywinauto.application import Application
 from PIL import Image, ImageGrab, ImageDraw
 import pyautogui, cv2, time, numpy, mss
@@ -22,22 +25,18 @@ def screenGrab():
     return 'full_snap__' + str(int(time.time()))+'.png'
 
 def checkSimilarity(img,img2):
-    sift = cv2.xfeatures2d.SIFT_create()
-    kp_1, desc_1 = sift.detectAndCompute(img, None)
-    kp_2, desc_2 = sift.detectAndCompute(img2, None)
-    index_params = dict(algorithm = 0, trees = 5)
-    good_points = []
-    search_params = dict()
-    flann = cv2.FlannBasedMatcher(index_params,search_params)
-    try:
-        matches = flann.knnMatch(desc_1,desc_2,k=2)
-        ratio = 0.6
-        for m, n in matches:
-            if m.distance < ratio*n.distance:
-                good_points.append(m)
-    except:
+    master_file = img
+    black1 = numpy.array([0,0,0], dtype=numpy.uint8)
+    black2 = numpy.array([0,0,0], dtype=numpy.uint8)
+    mask = cv2.inRange(img2, black1, black2)
+    cv2.imshow('m',mask)
+    compare_file = img2
+    difference = cv2.subtract(master_file,compare_file)
+    r,g,b = cv2.split(difference)
+    if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
         return True
-    return False
+    else:
+        return False
 
 
 def detChange(img):
@@ -50,35 +49,34 @@ def detChange(img):
 
 
 def screen_record():
-    mon = {"top": 600, "left": 250, "width": 50, "height": 100}
+    mon = {"top": 600, "left": 250, "width": 100, "height": 100}
     title = ""
     fps = 0
     sct = mss.mss()
     last_time = time.time()
-    mouse = Controller()
     while True:
         img = numpy.asarray(sct.grab(mon))
         fps += 1
-        img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        cv2.imwrite('pic.png',img)
-        detChange(img)
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+        lower_white = numpy.array([0,0,0], dtype=numpy.uint8)
+        upper_white = numpy.array([0,0,255], dtype=numpy.uint8)
+        mask = cv2.inRange(img, lower_white, upper_white)
+        cv2.imwrite('pic.png',mask)
         if detChange(img):
             mouse.position = (965,967)
             mouse.click(Button.left,1)
             time.sleep(2)
             mouse.click(Button.left,1)
-            time.sleep(5)
-            cv2.imwrite('pic2.png',img)
+            time.sleep(2)
+            cv2.imwrite('pic2.png',mask)
         else:
             pass
-##        lower = numpy.array([100,0,0])
-##        upper= numpy.array([255,255,255])
-##        mask = cv2.inRange(img,lower,upper)
-##        res = cv2.bitwise_and(img,img, mask= mask)
-        cv2.imshow('hello',img)
+        cv2.imwrite('pic2.png',mask)
+        cv2.imshow('Bobber cam',mask)
         k = cv2.waitKey(5) & 0xFF
         if k == 27:
             break
     return True
 
 screen_record()
+
